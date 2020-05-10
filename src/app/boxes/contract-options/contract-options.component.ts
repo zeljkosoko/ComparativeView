f95765c5365a222715cbcd3cd69b62007b71aeb4
Zeljkos <zeljkos@ever-near.com>
2020-05-10 22:59:31 +0200

    contract -options getPackages

--- src/app/boxes/contract-options/contract-options.component.ts
+++ src/app/boxes/contract-options/contract-options.component.ts
@@ -8,11 +8,15 @@ import { BoxPackagesService } from "../../box-packages.service";
 })
 export class ContractOptionsComponent implements OnInit {
   contract_length = {};
+  contractType:string = "";
+  items = [];
+  selectedItems = [];
 
   constructor(public boxPackagesService: BoxPackagesService) { }
 
   ngOnInit() {
     this.fillContractOpts();
+    this.fillItems();
   }
 
   fillContractOpts(): void{
@@ -20,5 +24,20 @@ export class ContractOptionsComponent implements OnInit {
     .subscribe(boxes => {
       this.contract_length = boxes["contract_length"];
     })
+  }
+
+  fillItems():void {
+    this.boxPackagesService.getBoxPackages()
+    .subscribe(boxes => {
+      this.items = boxes["items"];
+    })
+  }
+
+  getPackages(ev: any){
+    this.contractType = ev.target.value;
+    this.selectedItems = this.items.filter(item => 
+      item.prices["old_price_recurring"][this.contractType] 
+      || item.prices["price_recurring"][this.contractType]);
+    console.log(this.selectedItems);
   }
 }
